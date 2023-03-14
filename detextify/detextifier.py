a89cff189ee98b8d6aec8c1a59fbf3aa0e0a1152
Jay Hack <jayhack@users.noreply.github.com>
2023-03-14 11:01:03 -0700

    Add replace_text parameter to Detextify.detextify

--- detextify/detextifier.py
+++ detextify/detextifier.py
@@ -11,6 +11,7 @@ class Detextifier:
 
     def detextify(self, in_image_path: str, out_image_path: str, prompt=Inpainter.DEFAULT_PROMPT, max_retries=5):
         to_inpaint_path = in_image_path
+        replace_text = None
         for i in range(max_retries):
             print(f"Iteration {i} of {max_retries} for image {in_image_path}:")
 
@@ -21,9 +22,12 @@ class Detextifier:
                 for text_box in text_boxes:
                     cv2.putText(to_inpaint_path, prompt, (text_box.x, text_box.y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
             if not text_boxes:
-                break
+            if not text_boxes:
                 break
 
+            if replace_text is not None:
+                for text_box in text_boxes:
+                    cv2.putText(to_inpaint_path, replace_text, (text_box.x, text_box.y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
             print(f"\tCalling in-painting model...")
             self.inpainter.inpaint(to_inpaint_path, text_boxes, prompt, out_image_path)
             import os
